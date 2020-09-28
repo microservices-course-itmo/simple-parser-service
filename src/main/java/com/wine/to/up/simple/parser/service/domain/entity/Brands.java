f5e78258f3ba2c6d570bc1a827c443d49f3e6915
Murwar <40199868+Murwar@users.noreply.github.com>
2020-09-29 02:01:32 +0300

    Sprint1_Classes and Controller Creation
    Add Brands, Countries, Grapes, WineGrapesInfo, Wine Classes. The controller has been created. Controller Connection tested through swagger-UI.

---
+++ src/main/java/com/wine/to/up/simple/parser/service/domain/entity/Brands.java
@@ -0,0 +1,37 @@
+package com.wine.to.up.simple.parser.service.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.FileWriter;
+import java.util.UUID;
+
+@Entity
+@Table(name = "brands")
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class Brands {
+    @Id
+    @Column(name = "brandid")
+    private UUID brandID = UUID.randomUUID();
+
+    @Column(name = "brandname")
+    private String brandName;
+
+    public Brands(String grapeName) {
+        this.brandName = grapeName;
+    }
+
+    @SneakyThrows
+    public void writeInfoToFile(Brands someBrand){
+        FileWriter writer = new FileWriter("Brands.txt", false);
+        writer.write(someBrand.toString() + "\n");
+        writer.flush();
+    }
+
+}
