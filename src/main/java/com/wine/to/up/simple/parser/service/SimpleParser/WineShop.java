51be7da62b9f6b04239008001e8c8781e87d2d94
Olga <olga.molchanova.olkamana@gmail.com>
2020-10-03 20:52:44 +0300

    Requested changes for sprint 1
    WineShop class updated:
    + lombok annotations added
    + infoToFile() method changed
    
    Test related updates:
    + WineShopTest moved to test folder
    + New test added
    + JUnit dependency removed from pom.xml

--- src/main/java/com/wine/to/up/simple/parser/service/SimpleParser/WineShop.java
+++ src/main/java/com/wine/to/up/simple/parser/service/SimpleParser/WineShop.java
@@ -1,19 +1,22 @@
 package com.wine.to.up.simple.parser.service.SimpleParser;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+@ToString
+@Getter
+@Slf4j
 public class WineShop {
     private int shopID;
     private String shopName;
     private String shopAddress;
     private String shopPhone;
     private String shopWorkingHours;
-    private Logger logger;
 
 
     public WineShop(int shopID, String shopName, String shopAddress, String shopPhone, String shopWorkingHours) {
@@ -22,92 +25,58 @@ public class WineShop {
         this.shopAddress = shopAddress;
         this.shopPhone = shopPhone;
         this.shopWorkingHours = shopWorkingHours;
-        logger = LoggerFactory.getLogger("ShopLogger");
-        logger.info("The following object was created: " + this.toString());
+        log.info("New WineShop object was created");
+        infoToLog();
     }
 
-    @Override
-    public String toString() {
-        return "WineShop{" +
-                "shopID:" + this.shopID +
-                "; shopName:" + this.shopName +
-                "; shopAddress:" + this.shopAddress +
-                "; shopPhone:" + this.shopPhone +
-                "; shopWorkingHours:" + this.shopWorkingHours +
-                "}";
-    }
 
-    //log object info
+    //log object's current state
     public void infoToLog() {
-        logger.info(this.toString());
+        log.info("Current object state: " + this.toString());
     }
+
 
     //write object info to file
     public void infoToFile(File file) {
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(file, true);
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
             fileWriter.write(this.toString());
-
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                assert fileWriter != null;
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    public int getShopID() {
-        return shopID;
-    }
 
     public void setShopID(int shopID) {
-        logger.info("shopID was changed from " + this.shopID + " to " + shopID);
+        log.info("shopID was changed from \"{}\" to \"{}\"", this.shopID, shopID);
         this.shopID = shopID;
-        logger.info("Updated object: " + this.toString());
+        infoToLog();
     }
 
-    public String getShopName() {
-        return shopName;
-    }
 
     public void setShopName(String shopName) {
-        logger.info("shopName was changed from " + this.shopName + " to " + shopName);
+        log.info("shopName was changed from \"{}\" to \"{}\"", this.shopName, shopName);
         this.shopName = shopName;
-        logger.info("Updated object: " + this.toString());
+        infoToLog();
     }
 
-    public String getShopAddress() {
-        return shopAddress;
-    }
 
     public void setShopAddress(String shopAddress) {
-        logger.info("shopAddress was changed from " + this.shopAddress + " to " + shopAddress);
+        log.info("shopAddress was changed from \"{}\" to \"{}\"", this.shopAddress, shopAddress);
         this.shopAddress = shopAddress;
-        logger.info("Updated object: " + this.toString());
+        infoToLog();
     }
 
-    public String getShopPhone() {
-        return shopPhone;
-    }
 
     public void setShopPhone(String shopPhone) {
-        logger.info("shopPhone was changed from " + this.shopPhone + " to " + shopPhone);
+        log.info("shopPhone was changed from \"{}\" to \"{}\"", this.shopPhone, shopPhone);
         this.shopPhone = shopPhone;
-        logger.info("Updated object: " + this.toString());
+        infoToLog();
     }
 
-    public String getShopWorkingHours() {
-        return shopWorkingHours;
-    }
 
     public void setShopWorkingHours(String shopWorkingHours) {
-        logger.info("shopWorkingHours was changed from " + this.shopWorkingHours + " to " + shopWorkingHours);
+        log.info("shopWorkingHours was changed from \"{}\" to \"{}\"", this.shopWorkingHours, shopWorkingHours);
         this.shopWorkingHours = shopWorkingHours;
-        logger.info("Updated object: " + this.toString());
+        infoToLog();
     }
 }
