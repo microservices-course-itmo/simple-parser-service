9c76676e6ba1ed62a5713bf2c5942c198bb9a6c9
Olga <olga.molchanova.olkamana@gmail.com>
2020-10-05 21:35:22 +0300

    ShopCatalogTest added
    + edited ShopCatalog class

--- src/main/java/com/wine/to/up/simple/parser/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/simple/parser/service/ServiceApplication.java
@@ -2,9 +2,7 @@ package com.wine.to.up.simple.parser.service;
 
 import com.wine.to.up.simple.parser.service.SimpleParser.Parser;
 
-import io.micrometer.core.instrument.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,11 +15,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Slf4j
 @EnableScheduling
-public class ServiceApplication  {
+public class ServiceApplication {
 
     private final Parser parser;
 
-    public ServiceApplication(Parser parser){
+    public ServiceApplication(Parser parser) {
         this.parser = parser;
     }
 
@@ -29,8 +27,9 @@ public class ServiceApplication  {
         SpringApplication.run(ServiceApplication.class, args);
     }
 
-    @Scheduled(fixedDelayString = "PT12H") //run once in 12 hours
-    void scheduledRunParser(){
+    @Scheduled(fixedDelayString = "PT12H")
+        //run once in 12 hours
+    void scheduledRunParser() {
         log.info("SCHEDULED PARSER START");
         parser.startParser();
     }
