1161b271569085ed03b8013f549ed4a334aa6b63
DeLonghi <40316930+DeLonghi@users.noreply.github.com>
2020-10-13 20:30:08 +0300

    Delete ParaserConfig.java

--- src/main/java/com/wine/to/up/simple/parser/service/configuration/ParaserConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wine.to.up.simple.parser.service.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "application.properties")
-public class ParaserConfig {
-}
