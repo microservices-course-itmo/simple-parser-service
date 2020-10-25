7228d8a965c7723b753a0c1e4ed2d3ff69b6cb94
Mike <37367675+chinarev@users.noreply.github.com>
2020-10-25 12:14:44 +0300

    fix kafka

--- src/main/java/com/wine/to/up/simple/parser/service/messaging/SmopikTopicKafkaMessageHandler.java
+++ src/main/java/com/wine/to/up/simple/parser/service/messaging/SmopikTopicKafkaMessageHandler.java
@@ -15,20 +15,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Component
 @Slf4j
 public class SmopikTopicKafkaMessageHandler implements KafkaMessageHandler<String> {
-    //private final MessageRepository messageRepository;
 
     private final AtomicInteger counter = new AtomicInteger(0);
-
-//    @Autowired
-//    public StringTopicKafkaMessageHandler(MessageRepository messageRepository) {
-//        this.messageRepository = messageRepository;
-//    }
 
     @Override
     public void handle(String message) {
         counter.incrementAndGet();
         log.info("Message received message of type {}, number of messages: {}", message.getClass().getSimpleName(),
                 counter.get());
-        //messageRepository.save(new Message(message));
     }
 }
