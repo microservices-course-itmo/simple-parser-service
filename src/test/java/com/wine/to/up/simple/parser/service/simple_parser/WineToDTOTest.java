bd483ea2515620b92123a655d9fafe8f17b75762
01gaM <olga.molchanova.olkamana@gmail.com>
2020-11-03 19:07:51 +0300

    Updated tests for WineToDTOTest
    Signed-off-by: 01gaM <olga.molchanova.olkamana@gmail.com>

--- src/test/java/com/wine/to/up/simple/parser/service/simple_parser/WineToDTOTest.java
+++ src/test/java/com/wine/to/up/simple/parser/service/simple_parser/WineToDTOTest.java
@@ -1,6 +1,8 @@
 package com.wine.to.up.simple.parser.service.simple_parser;
 
 import com.wine.to.up.parser.common.api.schema.UpdateProducts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.modelmapper.ModelMapper;
@@ -10,14 +12,11 @@ import java.util.Collections;
 import static org.junit.Assert.*;
 
 class WineToDTOTest {
-    @ParameterizedTest
-    @CsvSource({
-            "красное,полусухое,RED,MEDIUM_DRY",
-            "белое,сухое,WHITE,DRY",
-            "розовое,сладкое,ROSE,SWEET",
-            "оранжевое,полусладкое,ORANGE,MEDIUM"})
-    void getProtoWineTest(String colorType, String sugarType, UpdateProducts.Product.Color color, UpdateProducts.Product.Sugar sugar) {
-        SimpleWine wine = SimpleWine.builder().
+    private SimpleWine wine;
+
+    @BeforeEach
+    void initWine() {
+        wine = SimpleWine.builder().
                 name("Бин 50 Шираз").
                 brandID("Lindeman's").
                 countryID("Австралия").
@@ -25,9 +24,7 @@ class WineToDTOTest {
                 year(2018).
                 capacity((float) 0.75).
                 strength((float) 13.0).
-                color(colorType).
                 grapeSort(Collections.singleton("шираз")).
-                sugar(sugarType).
                 discount((float) 20.0).
                 region("Новый Южный Уэльс").
                 link("https://simplewine.ru/catalog/product/lindeman_s_bin_50_shiraz_2018_075/").
@@ -38,6 +35,18 @@ class WineToDTOTest {
                         "Среднетелое, насыщенное и хорошо структурированное во вкусе, с бархатистыми танинами и оттенками черной смородины, сливы и ванили в послевкусии.").
                 sparkling(false).
                 build();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "красное,полусухое,RED,MEDIUM_DRY",
+            "белое,сухое,WHITE,DRY",
+            "розовое,сладкое,ROSE,SWEET",
+            "оранжевое,полусладкое,ORANGE,MEDIUM",
+            "фиолетовое,горькое, RED, DRY"})
+    void testGetProtoWine(String colorType, String sugarType, UpdateProducts.Product.Color color, UpdateProducts.Product.Sugar sugar) {
+        wine.setColor(colorType);
+        wine.setSugar(sugarType);
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setSkipNullEnabled(true);
@@ -48,5 +57,15 @@ class WineToDTOTest {
                 .setColor(color)
                 .setSugar(sugar);
         assertEquals(expectedProduct.toString(), WineToDTO.getProtoWine(wine).toString());
+    }
+
+    @Test
+    void testGetProtoWineEmptyString() {
+        wine.setColor("");
+        wine.setSugar("");
+        UpdateProducts.Product product = WineToDTO.getProtoWine(wine);
+
+        assertEquals(0, product.getColor().getNumber());
+        assertEquals(0, product.getSugar().getNumber());
     }
 }
