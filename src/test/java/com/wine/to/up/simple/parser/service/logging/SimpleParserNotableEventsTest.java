e1a0d45b441fafaeae7590ef1ae3fb76fbfc1d78
01gaM <olga.molchanova.olkamana@gmail.com>
2020-11-22 01:19:54 +0300

    Added EventSerializerTest and SimpleParserNotableEventsTest Removed EventDeserializer.java
    Signed-off-by: 01gaM <olga.molchanova.olkamana@gmail.com>

---
+++ src/test/java/com/wine/to/up/simple/parser/service/logging/SimpleParserNotableEventsTest.java
@@ -0,0 +1,24 @@
+package com.wine.to.up.simple.parser.service.logging;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.Assert.*;
+
+class SimpleParserNotableEventsTest {
+    @ParameterizedTest
+    @CsvSource({"I_CONTROLLER_RECEIVED_MESSAGE,I_CONTROLLER_RECEIVED_MESSAGE",
+            "I_KAFKA_SEND_MESSAGE_SUCCESS,I_KAFKA_SEND_MESSAGE_SUCCESS",
+            "W_SOME_WARN_EVENT,W_SOME_WARN_EVENT"})
+    void testGetName(String expectedName, SimpleParserNotableEvents event){
+        assertEquals(expectedName, event.getName());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Message: {},I_CONTROLLER_RECEIVED_MESSAGE",
+            "Kafka send message: {},I_KAFKA_SEND_MESSAGE_SUCCESS",
+            "Warn situation. Description: {},W_SOME_WARN_EVENT"})
+    void testGetTemplate(String expectedTemplate, SimpleParserNotableEvents event){
+        assertEquals(expectedTemplate, event.getTemplate());
+    }
+}
