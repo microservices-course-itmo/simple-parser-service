91c3f77921680713c64aabeab6e4af0135965843
Murwar <40199868+Murwar@users.noreply.github.com>
2021-03-14 21:40:54 +0300

    Registration in Eureka

--- src/main/java/com/wine/to/up/simple/parser/service/ServiceApplication.java
+++ src/main/java/com/wine/to/up/simple/parser/service/ServiceApplication.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Slf4j
 @EnableScheduling
 @RequiredArgsConstructor
+@EnableDiscoveryClient
 public class ServiceApplication {
     private final ParserService parserService;
 
